6acaeaf15dab219d1ceb7b4a3ec4b66927001796
Uyedacb <uyedacb@uw.edu>
2019-02-18 22:25:48 -0800

    add goals to projectpage example

--- src/ProjectPage.js
+++ src/ProjectPage.js
@@ -20,23 +20,24 @@ export default class ProjectPage extends Component {
         <Container>
           <Row>
             <Col sm="12" className="text-center">
+              <h1>Piaro</h1>
               <img src={imgUrl} alt="Piaro"/>
             </Col>
           </Row>
           <Row className="text-center">
             <Col className="text-justify project-page">
-              <h1>Piaro</h1>
+
               <Row className="role-summary">
                 <Col>
-                  <h3>Role:</h3>
+                  <h3>Role</h3>
                   <p>Software Developer</p>
-                  <h3>Time:</h3>
+                  <h3>Timeline</h3>
                   <p>October 2018</p>
                 </Col>
                 <Col>
-                  <h3>Team:</h3>
+                  <h3>Team</h3>
                   <p><a href="danielnguyen.me" target="_blank">Daniel Nguyen</a> Elias Mendel</p>
-                  <h3>Tools:</h3>
+                  <h3>Tools</h3>
                   <p>Android Studio, Google Language API, Firebase</p>
                 </Col>
               </Row>
@@ -44,10 +45,12 @@ export default class ProjectPage extends Component {
               <h2>Description</h2>
               <p>Piaro is a translation messaging application</p>
               <h2>Goals</h2>
-              <p>the main goal was to create a way for people across the world talk</p>
-              <h2>Process</h2>
+              <p>
+                The main goal was to create a way for people across the world to talk to each other without dealing with language barriers.
+              </p>
               </div>
               <div>
+                <h2>Process</h2>
                 <h3>Planning</h3>
                 <p>put planning stuff here</p>
                 <h3>Testing</h3>
